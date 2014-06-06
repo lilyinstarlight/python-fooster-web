05606452bb4c428162e2be1d7f2251f570026fed
Foster McLane <fkmclane@gmail.com>
2014-06-06 15:44:06 -0400

    Add license to setup file

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 setup(	name='web.py',
 	version='0.1',
 	description='A simple, threading, RESTful web server in Python',
+	license='MIT',
 	author='Foster McLane',
 	author_email='fkmclane@gmail.com',
 	packages=['web'],)
