6914ba7aa0648b6ab6021acff950dd3103414dff
Foster McLane <fkmclane@gmail.com>
2014-07-10 13:20:35 -0400

    Quote type on TypeError

--- web/web.py
+++ web/web.py
@@ -171,7 +171,7 @@ class HTTPHeaders(object):
 	def set(self, key, value):
 		dict_key = key.lower()
 		if not isinstance(value, str):
-			raise TypeError('\'value\' can only be a str')
+			raise TypeError('\'value\' can only be of type \'str\'')
 		self.headers[dict_key] = value
 		self.headers_actual[dict_key] = key
 
