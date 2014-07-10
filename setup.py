e5775a6e7fddb061fb0245db8e4a687b3c953641
Foster McLane <fkmclane@gmail.com>
2014-07-10 15:14:33 -0400

    Do not support Python 3.2

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+import sys
+
+if sys.version_info < (3, 3):
+	print('Only Python 3.3 or later is supported')
+	sys.exit(1)
+
 from distutils.core import setup
 
 from web import name, version
