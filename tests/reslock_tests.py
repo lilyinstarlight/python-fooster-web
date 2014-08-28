c10592aa8a5b2592ec0e48485f3c9e58a036f436
Foster McLane <fkmclane@gmail.com>
2014-08-28 15:23:15 -0400

    Add reslock tests

--- tests/reslock_tests.py
+++ tests/reslock_tests.py
@@ -1,16 +1,87 @@
+import threading
+import time
+
 from web import web
 
 def test_acquire():
-	pass
+	reslock = web.ResLock()
+
+	reslock.acquire('/')
+
+	assert '/' in reslock.locks
+	assert '/' in reslock.locks_count
+	assert reslock.locks_count['/'] == 1
+
+	reslock.release('/')
+
+	assert '/' not in reslock.locks
+	assert '/' not in reslock.locks_count
 
 def test_acquire_exists():
-	pass
+	reslock = web.ResLock()
+
+	reslock.acquire('/')
+
+	def acquire_exists():
+		reslock.acquire('/')
+
+	thread = threading.Thread(target=acquire_exists)
+
+	thread.start()
+
+	#Wait a bit
+	time.sleep(0.1)
+
+	assert thread.is_alive()
+	assert reslock.locks_count['/'] == 2
+
+	reslock.release('/')
+
+	thread.join(timeout=1)
+
+	reslock.release('/')
+
+	assert '/' not in reslock.locks
+	assert '/' not in reslock.locks_count
 
 def test_release_no_exists():
-	pass
+	reslock = web.ResLock()
+
+	try:
+		reslock.release('/')
+		assert False
+	except KeyError:
+		pass
 
 def test_wait():
-	pass
+	reslock = web.ResLock()
+
+	reslock.wait('/')
+
+	assert '/' not in reslock.locks
+	assert '/' not in reslock.locks_count
 
 def test_wait_exists():
-	pass
+	reslock = web.ResLock()
+
+	reslock.acquire('/')
+
+	def wait_exists():
+		reslock.wait('/')
+
+	thread = threading.Thread(target=wait_exists)
+
+	thread.start()
+
+	#Wait a bit
+	time.sleep(0.1)
+
+	assert thread.is_alive()
+	assert reslock.locks_count['/'] == 1
+
+	reslock.release('/')
+
+	thread.join(timeout=1)
+
+	assert '/' not in reslock.locks
+	assert '/' not in reslock.locks_count
