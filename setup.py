c24c9297c4126703b3bffed0c2bde9e636469d29
Foster McLane <fkmclane@gmail.com>
2016-06-07 11:03:04 -0400

    update indentation in setup

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@
 import sys
 
 if sys.version_info < (3, 3):
-	print('Only Python 3.3 or later is supported')
-	sys.exit(1)
+    print('Only Python 3.3 or later is supported')
+    sys.exit(1)
 
 from distutils.core import setup
 
 from web import name, version
 
 setup(
-	name=name,
-	version=version,
-	description='A simple, threading, RESTful web server in Python',
-	license='MIT',
-	author='Foster McLane',
-	author_email='fkmclane@gmail.com',
-	packages=['web'],
+    name=name,
+    version=version,
+    description='A simple, threading, RESTful web server in Python',
+    license='MIT',
+    author='Foster McLane',
+    author_email='fkmclane@gmail.com',
+    packages=['web'],
 )
