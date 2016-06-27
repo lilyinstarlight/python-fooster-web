ed0a307e2beae127cfbbc2f7bbd3c334be3273af
Foster McLane <fkmclane@gmail.com>
2016-06-27 12:19:44 -0400

    update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-import sys
-
-if sys.version_info < (3, 3):
-    print('Only Python 3.3 or later is supported')
-    sys.exit(1)
-
 from distutils.core import setup
 
 from web import name, version
@@ -12,7 +6,7 @@ from web import name, version
 setup(
     name=name,
     version=version,
-    description='A simple, threading, RESTful web server in Python',
+    description='a simple, threading, RESTful web server in Python',
     license='MIT',
     author='Foster McLane',
     author_email='fkmclane@gmail.com',
