6ef3528ec0ef16d17e34065a2dd8d2596cebac0c
Foster McLane <fkmclane@gmail.com>
2016-07-13 08:52:04 -0400

    add error page handler

--- web/page.py
+++ web/page.py
@@ -15,3 +15,29 @@ class PageHandler(web.HTTPHandler):
             page = file.read()
 
         return 200, self.format(page)
+
+
+class ErrorPageHandler(web.HTTPHander):
+    directory = '.'
+    page = 'error.html'
+
+    def format(self, page):
+        if self.error.status_message:
+            status_message = self.error.status_message
+        else:
+            status_message = web.status_messages[self.error.code]
+
+        if self.error.message:
+            message = self.error.message
+        else:
+            message = str(self.error.code) + ' - ' + status_message + '\n'
+
+        return page.format(code=self.error.code, status_message=status_message, message=message)
+
+    def respond(self):
+        self.response.headers.set('Content-Type', 'text/html')
+
+        with open(self.directory + '/' + self.page, 'r') as file:
+            page = file.read()
+
+        return 200, self.format(page)
