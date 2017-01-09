fbbc75965487d3f1c2d4dbfb2dc6dc482566a636
Foster McLane <fkmclane@gmail.com>
2017-01-09 11:14:16 -0500

    fix query mixin order

--- web/query.py
+++ web/query.py
@@ -4,7 +4,7 @@ import urllib.parse
 import web
 
 
-regex = '\?([\w=&])'
+regex = '(?:\?([\w=&]*))?'
 
 
 class QueryMixIn:
@@ -13,9 +13,11 @@ class QueryMixIn:
     def respond(self):
         self.request.query = dict(urllib.parse.parse_qsl(self.groups[self.group], True))
 
+        return super().respond()
+
 
 def new(base, handler):
-    class GenQueryHandler(handler, QueryMixIn):
+    class GenQueryHandler(QueryMixIn, handler):
         pass
 
     GenQueryHandler.group = re.compile(base).groups + 1
