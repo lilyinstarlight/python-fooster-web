3e595d1652cb39cd4b9887ff9df55effe3664a08
Foster McLane <fkmclane@gmail.com>
2017-08-01 16:16:56 -0400

    round out coverage for page.py

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,4 +1,4 @@
-from web import page
+from web import web, page
 
 import fake
 
@@ -8,11 +8,16 @@ import pytest
 test_string = 'hello {}'
 test_fill = 'world'
 
+test_error = '{message}'
+
 
 @pytest.fixture(scope='function')
 def tmp(tmpdir):
     with tmpdir.join('test.html').open('w') as file:
         file.write(test_string)
+
+    with tmpdir.join('error.html').open('w') as file:
+        file.write(test_error)
 
     return str(tmpdir)
 
@@ -48,3 +53,47 @@ def test_page_format(tmp):
 
     assert response[0] == 200
     assert response[1] == test_string.format(test_fill)
+
+
+def test_page_new_error():
+    assert page.new_error() == {'[0-9]{3}': page.PageErrorHandler}
+
+
+def test_page_error(tmp):
+    class TestHandler(page.PageErrorHandler):
+        directory = tmp
+
+        def respond(self):
+            self.error = web.HTTPError(500)
+
+            return super().respond()
+
+    request = fake.FakeHTTPRequest(None, ('', 0), None, handler=TestHandler)
+
+    headers, response = request.response.headers, request.handler.respond()
+
+    assert headers.get('Content-Type').startswith('text/html; charset=')
+
+    import sys
+    sys.stderr.write(repr(response[1]))
+    assert response[0] == 500
+    assert response[1] == '500 - Internal Server Error\n'
+
+
+def test_page_error_message(tmp):
+    class TestHandler(page.PageErrorHandler):
+        directory = tmp
+
+        def respond(self):
+            self.error = web.HTTPError(500, message=test_fill, status_message='a')
+
+            return super().respond()
+
+    request = fake.FakeHTTPRequest(None, ('', 0), None, handler=TestHandler)
+
+    headers, response = request.response.headers, request.handler.respond()
+
+    assert headers.get('Content-Type').startswith('text/html; charset=')
+
+    assert response[0] == 500
+    assert response[1] == test_fill
